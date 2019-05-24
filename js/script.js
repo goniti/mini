68b4ebd9fab04c5ce87d7aaf56a0baf17611c57e
Carlos <goniti@live.fr>
2019-05-24 15:28:12 +0200

    add section references

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
-const nav = $('.navbar__link');
+//initialize
+$(document).ready(function () {
+    //Navbar
+    const nav = $('.navbar__link');
+    nav.click(function () {
+        $(this).addClass('navbar__link--is-active').siblings().removeClass('navbar__link--is-active');
+    });
 
-nav.click(function() {
-
-    $(this).addClass('navbar__link--is-active').siblings().removeClass('navbar__link--is-active');
+    //Carousel
+    let mySwiper = new Swiper('.references__swiper-container', {
+        // Optional parameters
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        centeredSlides: true,
+        mousewheel: true,
+        loop: true,
+    })
 
 });
+
