9c873b8e4ed592aa4c033407621cb22a7da160c1
Carlos <goniti@live.fr>
2019-06-06 13:59:47 +0200

    add gallery

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
         },
         keyboard: {
             enabled: true,
-          },
+        },
         loop: true,
         spaceBetween: 30,
         effect: 'fade',
@@ -43,12 +43,41 @@ $(document).ready(function () {
     let portfolioSwiper = new Swiper('.portfolio__swiper-container', {
         direction: 'vertical',
         slidesPerView: 1,
-        spaceBetween: 30,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
+        spaceBetween: 60,
+        effect: 'fade',
         mousewheel: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.portfolio__btn-next',
+            prevEl: '.portfolio__btn-previous',
+        },
     });
-});
 
+    $(".portfolio__btn-close").click(function () {
+        portfolioSwiper.on('click', function (e, h) {
+            var el = e.target || e.currentTarget;
+            el.remove()
+            this.update()
+        });
+    });
+
+    //project toolbar
+    let galleryCategory = "";
+    $(".project__item").click(function () {
+        galleryCategory = this.id;
+        $(".project__columns").fadeTo(150, 0.15);
+        $(".project__columns div").not("." + galleryCategory).fadeOut().removeClass('project__column-show');
+        setTimeout(function () {
+            $("." + galleryCategory).fadeIn().addClass('project__column-show');
+            $(".project__columns").fadeTo(300, 1);
+        }, 300);
+
+    });
+// GALLERY ON CLICK 
+
+
+
+});
