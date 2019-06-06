f92b0dd4a8b944c8f999d87084ac7794acb399bc
Carlos <goniti@live.fr>
2019-06-06 16:21:49 +0200

    add overlay gallery

--- js/script.js
+++ js/script.js
@@ -78,6 +78,22 @@ $(document).ready(function () {
     });
 // GALLERY ON CLICK 
 
+const buttons = document.querySelectorAll('.project__column');
+const overlay = document.querySelector('.overlay');
+const overlayImage = document.querySelector('.overlay__inner img');
 
+function open(e) {
+  overlay.classList.add('open');
+  const src= e.currentTarget.querySelector('img').src;
+  overlayImage.src = src;
+}
+
+function close() {
+  overlay.classList.remove('open');
+}
+
+buttons.forEach(button => button.addEventListener('click', open));
+overlay.addEventListener('click', close);
 
 });
+
