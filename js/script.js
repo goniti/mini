da69facf0944a8f308d54269b5a36d366454d281
Carlos <goniti@live.fr>
2019-07-04 22:02:12 +0200

    add section client, blog, new

--- js/script.js
+++ js/script.js
@@ -1,8 +1,96 @@
 //initialize
 $(document).ready(function () {
+    /*---------------------SLIDER PARAMETERS---------------------*/
+    //--header Carousel
+    let mainSwiper = new Swiper('.main__swiper-container', {
+        // Optional parameters
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 3500,
+            disableOnInteraction: false,
+        },
+        keyboard: {
+            enabled: true,
+        },
+        loop: true,
+        spaceBetween: 30,
+        effect: 'fade',
+    })
 
-    /* TEST */
+    //--references Carousel
+    let referencesSwiper = new Swiper('.references__swiper-container', {
+        // Optional parameters
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        loop: true,
+    })
 
+    //--portfolio Carousel
+    let portfolioSwiper = new Swiper('.portfolio__swiper-container', {
+        direction: 'vertical',
+        slidesPerView: 1,
+        spaceBetween: 60,
+        effect: 'fade',
+        mousewheel: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.portfolio__btn-next',
+            prevEl: '.portfolio__btn-previous',
+        },
+    });
+
+
+    /*---------------------SCRIPTS------------------------*/
+
+    //BURGER-MENU
+    const page = $('.page__content');
+    const burgerMenu = $('.burgermenu');
+    const burgerMenuBtnOpen = $('.burgermenu__btn--open');
+    const burgerMenuBtnClose = $('.burgermenu__btn--close');
+    const burgerLink = $('.burgermenu__list--item');
+
+
+    burgerLink.click(function () {
+        $(this).addClass('burgermenu__link--is-active').siblings().removeClass('burgermenu__link--is-active');
+    });
+
+    //--Open burger menu
+    $(burgerMenuBtnOpen).on('click', function () {
+        $(burgerMenuBtnOpen).addClass('js-hidden');
+        $(burgerMenu).addClass('js-opened');
+        $(page).addClass('js-opened');
+
+    });
+
+    //--Close burger menu
+    $(burgerMenuBtnClose).on('click', function () {
+        $(burgerMenuBtnOpen).removeClass('js-hidden');
+        $(burgerMenu).removeClass('js-opened');
+        $(page).removeClass('js-opened');
+    });
+    //--Click outside close burger menu
+    $(page).on("click", function () {
+        $(burgerMenuBtnOpen).removeClass('js-hidden');
+        $(burgerMenu).removeClass('js-opened');
+        $(page).removeClass('js-opened');
+    });
+    //Navbar
+    const nav = $('.navbar__link');
+    nav.click(function () {
+        $(this).addClass('navbar__link--is-active').siblings().removeClass('navbar__link--is-active');
+    });
 
     $(window).scroll(function () {
         //value >= 100 (1 scroll) value > 100 (2 scroll)
@@ -11,140 +99,48 @@ $(document).ready(function () {
         } else {
             $(".navbar").removeClass("navbar__menu-background");
         }
-    
-
-    if ($(document).scrollTop() > 800) {
 
 
-        $(".navbar").addClass("navbar__menu-scrolled");
-    } else {
-        $(".navbar").removeClass("navbar__menu-scrolled");
+        if ($(document).scrollTop() > 900) {
+
+
+            $(".navbar").addClass("navbar__menu-scrolled");
+        } else {
+            $(".navbar").removeClass("navbar__menu-scrolled");
+        }
+    });
+
+    //gallery toolbar
+    let galleryCategory = "";
+    $(".gallery__item").click(function () {
+        galleryCategory = this.id;
+        $(".gallery__columns").fadeTo(150, 0.15);
+        $(".gallery__column").not("." + galleryCategory).fadeOut().removeClass('gallery__column-show');
+        setTimeout(function () {
+            $("." + galleryCategory).fadeIn().addClass('gallery__column-show');
+            $(".gallery__columns").fadeTo(300, 1);
+        }, 300);
+
+    });
+
+    // GALLERY ON CLICK 
+    const buttons = document.querySelectorAll('.gallery__column');
+    const overlay = document.querySelector('.overlay');
+    const overlayImage = document.querySelector('.overlay__inner img');
+
+    function open(e) {
+        overlay.classList.add('open');
+        const src = e.currentTarget.querySelector('img').src;
+        overlayImage.src = src;
     }
-});
 
-//BURGER-MENU
-const burgerMenu = $('.burgerMenu');
-const burgerMenuBtnOpen = $('.burgerMenu__btn--open');
-const burgerMenuBtnClose = $('.burgerMenu__btn--close');
-const page = $('.page__content');
+    function close() {
+        overlay.classList.remove('open');
+    }
 
-// Open burger menu
-$(burgerMenuBtnOpen).on('click', function () {
-    $(burgerMenuBtnOpen).addClass('js-hidden');
-    $(burgerMenu).addClass('js-opened');
-    $(page).addClass('js-opened');
+    buttons.forEach(button => button.addEventListener('click', open));
+    overlay.addEventListener('click', close);
 
-});
-
-// Close burger menu
-$(burgerMenuBtnClose).on('click', function () {
-    $(burgerMenuBtnOpen).removeClass('js-hidden');
-    $(burgerMenu).removeClass('js-opened');
-    $(page).removeClass('js-opened');
-});
-// Click outside close burger menu
-$(page).on("click", function () {
-    $(burgerMenuBtnOpen).removeClass('js-hidden');
-    $(burgerMenu).removeClass('js-opened');
-    $(page).removeClass('js-opened');
-});
-/* TEST */
-
-
-
-
-
-
-
-// SLIDER PARAMETERS
-//--header Carousel
-let mainSwiper = new Swiper('.main__swiper-container', {
-    // Optional parameters
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    autoplay: {
-        delay: 3500,
-        disableOnInteraction: false,
-    },
-    keyboard: {
-        enabled: true,
-    },
-    loop: true,
-    spaceBetween: 30,
-    effect: 'fade',
-})
-
-//--references Carousel
-let referencesSwiper = new Swiper('.references__swiper-container', {
-    // Optional parameters
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    loop: true,
-})
-
-//--portfolio Carousel
-let portfolioSwiper = new Swiper('.portfolio__swiper-container', {
-    direction: 'vertical',
-    slidesPerView: 1,
-    spaceBetween: 60,
-    effect: 'fade',
-    mousewheel: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.portfolio__btn-next',
-        prevEl: '.portfolio__btn-previous',
-    },
-});
-
-
-// SCRIPTS
-//Navbar
-const nav = $('.navbar__link');
-nav.click(function () {
-    $(this).addClass('navbar__link--is-active').siblings().removeClass('navbar__link--is-active');
-});
-
-//gallery toolbar
-let galleryCategory = "";
-$(".gallery__item").click(function () {
-    galleryCategory = this.id;
-    $(".gallery__columns").fadeTo(150, 0.15);
-    $(".gallery__column").not("." + galleryCategory).fadeOut().removeClass('gallery__column-show');
-    setTimeout(function () {
-        $("." + galleryCategory).fadeIn().addClass('gallery__column-show');
-        $(".gallery__columns").fadeTo(300, 1);
-    }, 300);
-
-});
-
-// GALLERY ON CLICK 
-const buttons = document.querySelectorAll('.gallery__column');
-const overlay = document.querySelector('.overlay');
-const overlayImage = document.querySelector('.overlay__inner img');
-
-function open(e) {
-    overlay.classList.add('open');
-    const src = e.currentTarget.querySelector('img').src;
-    overlayImage.src = src;
-}
-
-function close() {
-    overlay.classList.remove('open');
-}
-
-buttons.forEach(button => button.addEventListener('click', open));
-overlay.addEventListener('click', close);
 
 });
 
